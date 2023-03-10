d816783e36bc6392d5c61f91fe0b0ad756c3783b
Flimill <uhovad03@gmail.com>
2023-03-11 01:19:39 +0300

    2.17 #2

--- js/main.js
+++ js/main.js
@@ -1,16 +1,16 @@
 function getRandomNumber(from, to){
   if (from >= to){
-    return "Invalid input values";
+    return 'Invalid input values';
   }
 
   return Math.random()*(to - from) + from;
 }
-console.log(getRandomNumber(1,9))
+console.log(getRandomNumber(1,9));
 
 function checkStringLength(string, Maxlength){
 
   return String(string).length <= Maxlength;
 }
 
-console.log(checkStringLength("What is it?",2))
-console.log(checkStringLength("What is it?",11))
+console.log(checkStringLength('What is it?',2));
+console.log(checkStringLength('What is it?',11));
