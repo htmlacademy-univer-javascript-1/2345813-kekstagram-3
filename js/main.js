4cf5675ca07b71d6f358c7e7e210cced59ba17be
Flimill <uhovad03@gmail.com>
2023-03-11 04:03:20 +0300

    4.15

--- js/main.js
+++ js/main.js
@@ -1,15 +1,27 @@
-function getRandomNumber(from, to){
-  if (from >= to){
-    return 'Invalid input values';
+function getRandomNumber(a, b){
+  const lower = Math.ceil(Math.min(Math.abs(a), Math.abs(b)));
+  const upper = Math.floor(Math.max(Math.abs(a), Math.abs(b)));
+  const result = Math.random() * (upper - lower + 1) + lower;
+  return Math.floor(result);
+}
+
+function checkStringLength (string, length) {
+  return string.length <= length;
+}
+
+function generatePhotos(number){
+  let photos = new Array(number);
+  descritions = ['beautiful', 'sad', 'funny', 'sad', 'boring'];
+
+  for (let i = 1; i<=number; i++){
+    photos[i-1] = {
+      id: i,
+      url: 'photos/' + i,
+      descrition: descritions[i % 5],
+      likes: getRandomNumber(15,200),
+      comments: getRandomNumber(0,200),
+    }
   }
-
-  return Math.random()*(to - from) + from;
+  return photos
 }
-getRandomNumber(1,9);
-
-function checkStringLength(string, Maxlength){
-
-  return String(string).length <= Maxlength;
-}
-
-checkStringLength('What is it?',11);
+console.log(generatePhotos(25)[1]);
