17599c6f2ff550ab50af412f76859c0360428d88
Flimill <uhovad03@gmail.com>
2023-03-24 22:57:42 +0300

    17.15 #2

--- js/main.js
+++ js/main.js
@@ -1,6 +1,6 @@
-console.log(1);
 import { generateRandomPhotos } from "./data.js";
-import { drawPhoto } from "./thumbailRendering.js";
+import { drawPhotos } from "./thumbailRendering.js";
 
 let photos = generateRandomPhotos(25);
-drawPhoto(photos[1])
+drawPhotos(photos);
+
--- js/thumbailRendering.js
+++ js/thumbailRendering.js
@@ -1,9 +1,17 @@
-import { generateRandomPhotos } from './data.js';
+function drawPhotos(photos) {
+  const fragment = document.createDocumentFragment();
+  const template = document.querySelector('#picture');
+  photos.forEach(photo => {
+    const picture = template.content.cloneNode(true);
 
+    picture.querySelector('.picture__img').src = photo.url;
+    picture.querySelector('.picture__comments').textContent = photo.comments;
+    picture.querySelector('.picture__likes').textContent = photo.likes;
 
-
-function drawPhoto(photo){
-    const template = document.querySelector('#picture')
-    console.log(template.content)
+    fragment.appendChild(picture);
+  });
+  const photoBlock = document.querySelector('.pictures');
+  photoBlock.appendChild(fragment);
 }
-export {drawPhoto};
+
+export { drawPhotos };
