18ca727b947b5e36259e5fe324e8aeb9f242eb30
Flimill <uhovad03@gmail.com>
2023-03-25 11:22:33 +0300

    7.15 #6

--- js/data.js
+++ js/data.js
@@ -1,5 +1,7 @@
 import { getRandomNumber } from './util.js';
 
+const DESCRIPTIONS = ['beautiful', 'sad', 'funny', 'sad', 'boring'];
+
 function generatePhotoObject(id, url, descrition, likes, comments) {
   const photo = {
     id,
@@ -13,10 +15,9 @@ function generatePhotoObject(id, url, descrition, likes, comments) {
 
 function generateRandomPhotoObjects(number) {
   const photos = new Array(number);
-  const descritions = ['beautiful', 'sad', 'funny', 'sad', 'boring'];
 
   for (let i = 1; i <= number; i++) {
-    photos[i - 1] = generatePhotoObject(i, `photos/${i}.jpg`, descritions[i % 5], getRandomNumber(15, 200), getRandomNumber(0, 200));
+    photos[i - 1] = generatePhotoObject(i, `photos/${i}.jpg`, DESCRIPTIONS[i % 5], getRandomNumber(15, 200), getRandomNumber(0, 200));
   }
   return photos;
 }
