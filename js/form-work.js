b4908abdb2fdaa6f4433469715b71b1ca0e555d9
Flimill <uhovad03@gmail.com>
2023-04-01 06:15:08 +0300

    8.19 #3

--- js/form-work.js
+++ js/form-work.js
@@ -25,16 +25,16 @@ closeButton.addEventListener('click', closeImageEditor);
 
 window.onload = function () {
 
-  var form = document.getElementById(".text__description");
+  const form = document.getElementById(".text__description");
 
   // create the pristine instance
-  var pristine = new Pristine(form);
+  const pristine = new Pristine(form);
 
   form.addEventListener('submit', function (e) {
      e.preventDefault();
 
      // check if the form is valid
-     var valid = pristine.validate(); // returns true or false
+     const valid = pristine.validate(); // returns true or false
 
   });
 };
